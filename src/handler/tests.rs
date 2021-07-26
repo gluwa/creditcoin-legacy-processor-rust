5001ff5bb7fe81f7afe7d5eb978d5a9c67d67412
nathan.whitaker <nathan.whitaker01@gmail.com>
2021-07-26 12:54:35 -0400

    Allow serializing CCCommands for tests

--- src/handler/tests.rs
+++ src/handler/tests.rs
@@ -242,6 +242,200 @@ command!(Four, P1, P2, P3, P4);
 command!(Five, P1, P2, P3, P4, P5);
 command!(Six, P1, P2, P3, P4, P5, P6);
 
+trait ToGenericCommand {
+    type GenericCommand: Serialize;
+    fn to_generic_command(self) -> Self::GenericCommand;
+}
+
+impl ToGenericCommand for SendFunds {
+    type GenericCommand = TwoArgCommand;
+    fn to_generic_command(self) -> <Self as ToGenericCommand>::GenericCommand {
+        let SendFunds { amount, sighash } = self;
+        TwoArgCommand::new("SendFunds", amount.to_string(), sighash.0)
+    }
+}
+
+impl ToGenericCommand for RegisterAddress {
+    type GenericCommand = ThreeArgCommand;
+    fn to_generic_command(self) -> ThreeArgCommand {
+        let RegisterAddress {
+            blockchain,
+            address,
+            network,
+        } = self;
+        ThreeArgCommand::new("RegisterAddress", blockchain, address, network)
+    }
+}
+impl ToGenericCommand for RegisterTransfer {
+    type GenericCommand = ThreeArgCommand;
+    fn to_generic_command(self) -> ThreeArgCommand {
+        let RegisterTransfer {
+            gain,
+            order_id,
+            blockchain_tx_id,
+        } = self;
+        ThreeArgCommand::new(
+            "RegisterTransfer",
+            gain.to_string(),
+            order_id,
+            blockchain_tx_id,
+        )
+    }
+}
+impl ToGenericCommand for AddAskOrder {
+    type GenericCommand = SixArgCommand;
+    fn to_generic_command(self) -> SixArgCommand {
+        let AddAskOrder {
+            address_id,
+            amount_str,
+            interest,
+            maturity,
+            fee,
+            expiration,
+        } = self;
+        SixArgCommand::new(
+            "AddAskOrder",
+            address_id,
+            amount_str,
+            interest,
+            maturity,
+            fee,
+            expiration,
+        )
+    }
+}
+impl ToGenericCommand for AddBidOrder {
+    type GenericCommand = SixArgCommand;
+    fn to_generic_command(self) -> SixArgCommand {
+        let AddBidOrder {
+            address_id,
+            amount_str,
+            interest,
+            maturity,
+            fee,
+            expiration,
+        } = self;
+        SixArgCommand::new(
+            "AddBidOrder",
+            address_id,
+            amount_str,
+            interest,
+            maturity,
+            fee,
+            expiration,
+        )
+    }
+}
+impl ToGenericCommand for AddOffer {
+    type GenericCommand = ThreeArgCommand;
+    fn to_generic_command(self) -> ThreeArgCommand {
+        let AddOffer {
+            ask_order_id,
+            bid_order_id,
+            expiration,
+        } = self;
+        ThreeArgCommand::new("AddOffer", ask_order_id, bid_order_id, expiration)
+    }
+}
+impl ToGenericCommand for AddDealOrder {
+    type GenericCommand = TwoArgCommand;
+    fn to_generic_command(self) -> TwoArgCommand {
+        let AddDealOrder {
+            offer_id,
+            expiration,
+        } = self;
+        TwoArgCommand::new("AddDealOrder", offer_id, expiration)
+    }
+}
+impl ToGenericCommand for CompleteDealOrder {
+    type GenericCommand = TwoArgCommand;
+    fn to_generic_command(self) -> TwoArgCommand {
+        let CompleteDealOrder {
+            deal_order_id,
+            transfer_id,
+        } = self;
+        TwoArgCommand::new("CompleteDealOrder", deal_order_id, transfer_id)
+    }
+}
+impl ToGenericCommand for LockDealOrder {
+    type GenericCommand = OneArgCommand;
+    fn to_generic_command(self) -> OneArgCommand {
+        let LockDealOrder { deal_order_id } = self;
+        OneArgCommand::new("LockDealOrder", deal_order_id)
+    }
+}
+impl ToGenericCommand for CloseDealOrder {
+    type GenericCommand = TwoArgCommand;
+    fn to_generic_command(self) -> TwoArgCommand {
+        let CloseDealOrder {
+            deal_order_id,
+            transfer_id,
+        } = self;
+        TwoArgCommand::new("CloseDealOrder", deal_order_id, transfer_id)
+    }
+}
+impl ToGenericCommand for Exempt {
+    type GenericCommand = TwoArgCommand;
+    fn to_generic_command(self) -> TwoArgCommand {
+        let Exempt {
+            deal_order_id,
+            transfer_id,
+        } = self;
+        TwoArgCommand::new("Exempt", deal_order_id, transfer_id)
+    }
+}
+impl ToGenericCommand for AddRepaymentOrder {
+    type GenericCommand = FourArgCommand;
+    fn to_generic_command(self) -> FourArgCommand {
+        let AddRepaymentOrder {
+            deal_order_id,
+            address_id,
+            amount,
+            expiration,
+        } = self;
+        FourArgCommand::new(
+            "AddRepaymentOrder",
+            deal_order_id,
+            address_id,
+            amount,
+            expiration,
+        )
+    }
+}
+impl ToGenericCommand for CompleteRepaymentOrder {
+    type GenericCommand = OneArgCommand;
+    fn to_generic_command(self) -> OneArgCommand {
+        let CompleteRepaymentOrder { repayment_order_id } = self;
+        OneArgCommand::new("CompleteRepaymentOrder", repayment_order_id)
+    }
+}
+impl ToGenericCommand for CloseRepaymentOrder {
+    type GenericCommand = TwoArgCommand;
+    fn to_generic_command(self) -> TwoArgCommand {
+        let CloseRepaymentOrder {
+            repayment_order_id,
+            transfer_id,
+        } = self;
+        TwoArgCommand::new("CloseRepaymentOrder", repayment_order_id, transfer_id)
+    }
+}
+impl ToGenericCommand for CollectCoins {
+    type GenericCommand = ThreeArgCommand;
+    fn to_generic_command(self) -> ThreeArgCommand {
+        let CollectCoins {
+            eth_address,
+            amount,
+            blockchain_tx_id,
+        } = self;
+        ThreeArgCommand::new(
+            "CollectCoins",
+            eth_address,
+            amount.to_string(),
+            blockchain_tx_id,
+        )
+    }
+}
+
 #[track_caller]
 #[cfg(not(all(test, feature = "integration-testing")))]
 fn deserialize_success(value: impl Serialize, expected: impl Into<CCCommand>) {
@@ -1296,7 +1490,7 @@ fn execute_success(
 
 #[track_caller]
 fn execute_failure(
-    command: impl CCTransaction,
+    command: impl CCTransaction + ToGenericCommand,
     request: &TpProcessRequest,
     tx_ctx: &MockTransactionContext,
     ctx: &mut MockHandlerContext,
